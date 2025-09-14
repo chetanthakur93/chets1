002e96e0c2e8d4ba3d85af301210701f26bd152a
Chetan Thakur <you@example.com>
2025-09-14 13:17:33 +0530

    9thsept25

---
+++ SEPTEMBER/Python_Basics/SOURCE_C0DE.py
@@ -0,0 +1,16 @@
+print(2+2)
+print(2+2)
+print(2+2)
+print(27465446+3364646)
+print(2/2)
+
+# `print(*values, sep=" ", end="\n", file=None, flush=False)` on docs.python.org
+print(2/2)
+print("chetan",2,"thakur",sep="_",end="\n")
+
+print(2-7)
+print(2-2)
+print(2/2)
+import keyword
+print(keyword.kwlist)
+
---
+++ SEPTEMBER/Python_Basics/TEST-1.py
@@ -0,0 +1,23 @@
+
+age=31
+print(age)
+_ = 31
+_ = _+2
+print(_)
+pi = 3.14
+name = "chetan"
+date = 27-11-1993
+Raju = False
+print(type(pi))
+print(type(name))
+print(type(Raju))
+print(type(date))
+a=25
+b=25
+c=a+b
+print(c)
+result= max(23,34,43,67,90)
+print(result)
+Result= min(67,32,12,90,88,76,43,25,19272)
+print(Result)
+
