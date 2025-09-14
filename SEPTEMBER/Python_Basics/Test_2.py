69dc09a0c73bd31fae2cecd5be1884c92a23803b
Chetan Thakur <you@example.com>
2025-09-14 13:17:33 +0530

    9thsept25

---
+++ SEPTEMBER/Python_Basics/Test_2.py
@@ -0,0 +1,10 @@
+##write a program to 2 take two user input
+##build logic
+num1 = int(input ("enter the num 1"))
+num2 = int(input ("enter the num 2"))
+##print
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
