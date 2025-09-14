13eff051a883ff180c66b7545022400561949fe7
Chetan Thakur <you@example.com>
2025-09-14 19:07:04 +0530

    9thsept25

---
+++ SEPTEMBER/Python_Basics/Test_3.py
@@ -0,0 +1,24 @@
+name = "chetan"
+print(len(name))
+age= "23"
+print(type(age))
+age=56
+
+first_name= "chetan"
+last_name = "thakur"
+full_name = first_name + " " + last_name
+print (full_name )
+print(type(full_name))
+
+##  basic data types
+## int , str, bool
+
+#escape squence \n
+#   \t = backspace
+#  \b = backspace
+print("chetan\nthakur")
+print("Thakur\tgreat")
+print("thakur\bgreat")
+
+
+
